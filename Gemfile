e89537500dbe9183d9ed33a28d33181116ed60b7
Jon Fuller <jcfuller@sep.com>
2012-11-01 18:11:04 -0400

    add next holiday

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "http://rubygems.org"
 
 gem 'sinatra'
-gem 'sep_holidays', :git => 'git://github.com/sep/sep_holidays.git'
+gem 'sep_holidays', :git => 'git://github.com/jonfuller/sep_holidays.git', :branch => "next_holiday"
--- isitaholiday.rb
+++ isitaholiday.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
-require 'bundler/setup'
+require 'bundler'
+Bundler.setup
 require 'sinatra'
 require 'sep_holidays'
 
 get '/' do
- return "yep" if Date.today.is_sep_holiday?
- return "nope"
+ return "Yep!" if Date.today.is_sep_holiday?
+
+ next_holiday = Date.today.next_sep_holiday
+
+ return "Nope, but it'll be #{next_holiday.name} in #{(next_holiday.date - Date.today).to_i} days!"
 end
 
